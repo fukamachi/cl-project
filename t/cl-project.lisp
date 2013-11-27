d5718a77018e52f75a289ae0ad9e4751d934f4f0
Masataro Asai <guicho2.71828@gmail.com>
2013-11-27 15:10:18 +0900

    cleanup the test

--- t/cl-project.lisp
+++ t/cl-project.lisp
@@ -15,108 +15,109 @@
 (in-package :cl-project-test)
 
 
-(defvar *sample-project-directory*)
+(defvar *systems* nil)
 
-(let ((*sample-project-directory*
-       (asdf:system-relative-pathname
-        :cl-project
-        #p"t/sample/")))
-  (unwind-protect
-       (progn
-         (plan 2)
-         (when (file-exists-p *sample-project-directory*)
-           (delete-directory-and-files *sample-project-directory*))
-         (cl-project:make-project *sample-project-directory*)
-         (ok (directory-exists-p *sample-project-directory*)
-             "Sample project was generated")
-         (ok (asdf:load-system :sample)
-             "Can load the new project")
-         (finalize))
-    (delete-directory-and-files *sample-project-directory*)))
+(defun sys-push (path)
+  (let ((path2 (asdf:system-relative-pathname
+                :cl-project path)))
+    (push path2 *systems*)
+    path2))
+
+(let ((dir (sys-push #p"t/sample/")))
+  (plan 2)
+  (when (file-exists-p dir)
+    (delete-directory-and-files dir))
+  (cl-project:make-project dir)
+  (ok (directory-exists-p dir)
+      "Sample project was generated")
+  (ok (asdf:load-system :sample)
+      "Can load the new project")
+  (finalize))
 
 
-(let ((*sample-project-directory*
-       (asdf:system-relative-pathname
-        :cl-project
-        #p"t/sample-no-confirm/")))
-  (unwind-protect
-       (progn
-         (plan 2)
-         (when (file-exists-p *sample-project-directory*)
-           (delete-directory-and-files *sample-project-directory*))
-         (cl-project:make-project *sample-project-directory* :confirm nil)
-         (ok (directory-exists-p *sample-project-directory*)
-             "Sample project was generated")
-         (ok (asdf:load-system :sample-no-confirm)
-             "Can load the new project")
-         (finalize))
-    (delete-directory-and-files *sample-project-directory*)))
+(let ((dir (sys-push #p"t/sample-no-confirm/")))
+  (plan 2)
+  (when (file-exists-p dir)
+    (delete-directory-and-files dir))
+  (cl-project:make-project dir :confirm nil)
+  (ok (directory-exists-p dir)
+      "Sample project was generated")
+  (ok (asdf:load-system :sample-no-confirm)
+      "Can load the new project")
+  (finalize))
 
-(let ((*sample-project-directory*
-       (asdf:system-relative-pathname
-        :cl-project
-        #p"t/sample-mod/")))
-  (unwind-protect
-       (progn
-         (plan 2)
-         (cl-project:make-project *sample-project-directory*
-                                  :processor 'modified-processor
-                                  :author "alien tech")
-         (ok (directory-exists-p *sample-project-directory*)
-             "Sample project was generated")
-         (ok (asdf:load-system :sample-mod)
-             "Can load the new project")
-         (finalize))
-    (delete-directory-and-files *sample-project-directory*)))
+(let ((dir (sys-push #p"t/sample-mod/")))
+  (plan 2)
+  (when (file-exists-p dir)
+    (delete-directory-and-files dir))
+  (cl-project:make-project dir
+                           :processor 'modified-processor
+                           :author "alien tech")
+  (ok (directory-exists-p dir)
+      "Sample project was generated")
+  (ok (asdf:load-system :sample-mod)
+      "Can load the new project")
+  (finalize))
 
-(let ((*sample-project-directory*
-       (asdf:system-relative-pathname
-        :cl-project
-        #p"t/sample-eos/")))
-  (unwind-protect
-       (progn
-         (plan 2)
-         (cl-project:make-project *sample-project-directory*
-                                  :processor 'optima-like-processor)
-         (ok (directory-exists-p *sample-project-directory*)
-             "Sample project was generated")
-         (ok (asdf:load-system :sample-eos)
-             "Can load the new project")
-         (finalize))
-    (delete-directory-and-files *sample-project-directory*)))
+(let ((dir (sys-push #p"t/sample-eos/")))
+  (plan 2)
+  (when (file-exists-p dir)
+    (delete-directory-and-files dir))
+  (cl-project:make-project dir
+                           :processor 'optima-like-processor)
+  (ok (directory-exists-p dir)
+      "Sample project was generated")
+  (ok (asdf:load-system :sample-eos)
+      "Can load the new project")
+  (finalize))
 
-;; (let ((*sample-project-directory*
-;;        (asdf:system-relative-pathname
-;;         :cl-project
-;;         #p"t/sample-interactive/")))
-;;   (unwind-protect
-;;        (progn
-;;          (plan 2)
-;;          (cl-project:make-project *sample-project-directory*
-;;                                   :processor 'default-iprocessor)
-;;          (ok (directory-exists-p *sample-project-directory*)
-;;              "Sample project was generated")
-;;          (ok (asdf:load-system :sample-interactive)
-;;              "Can load the new project")
-;;          (finalize))
-;;     (delete-directory-and-files *sample-project-directory*)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; git
 
-(let ((*sample-project-directory*
-       (asdf:system-relative-pathname
-        :cl-project
-        #p"t/sample-git/")))
-  (unwind-protect
-       (progn
-         (plan 3)
-         (cl-project:make-project *sample-project-directory*
-                                  :processor 'git-processor)
-         (ok (directory-exists-p *sample-project-directory*)
-             "Sample project was generated")
-         (let ((git-dir (merge-pathnames ".git" *sample-project-directory*)))
-           (ok (directory-exists-p git-dir)
-               (format nil "Git repo ~a initialized" git-dir)))
-         (ok (asdf:load-system :sample-git)
-             "Can load the new project")
-         (finalize))
-    (delete-directory-and-files *sample-project-directory*)))
+(let ((dir (sys-push #p"t/sample-git/")))
+  (plan 3)
+  (when (file-exists-p dir)
+    (delete-directory-and-files dir))
+  (cl-project:make-project dir
+                           :processor 'git-processor)
+  (ok (directory-exists-p dir)
+      "Sample project was generated")
+  (let ((git-dir (merge-pathnames ".git" dir)))
+    (ok (directory-exists-p git-dir)
+        (format nil "Git repo ~a initialized" git-dir)))
+  (ok (asdf:load-system :sample-git)
+      "Can load the new project")
+  (finalize))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; interactive
+
+(defclass git-iprocessor (interactive-processor
+                          markdown-readme-mixin
+                          src-dir-mixin
+                          t-dir-mixin
+                          test-package-hyphened-mixin
+                          cl-test-more-mixin
+                          package-file-as-project-mixin
+                          git-mixin)
+  ())
+
+(let ((dir (sys-push #p"t/sample-interactive/")))
+  (plan 2)
+  (when (file-exists-p dir)
+    (delete-directory-and-files dir))
+  (cl-project:make-project dir
+                           :processor 'git-iprocessor)
+  (ok (directory-exists-p dir)
+      "Sample project was generated")
+  (ok (asdf:load-system :sample-interactive)
+      "Can load the new project")
+  (finalize))
+
+(defun clean ()
+  (dolist (dir (mapcar (lambda (pathname)
+                         (asdf:system-relative-pathname
+                          :cl-project pathname))
+                       *systems*))
+    (when (file-exists-p dir)
+      (delete-directory-and-files dir))))
