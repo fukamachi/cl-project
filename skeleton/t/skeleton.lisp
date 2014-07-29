492c49767e6610fad051e586776139af2c265696
Eitaro Fukamachi <e.arrows@gmail.com>
2014-07-29 20:41:27 +0900

    Remove a file header from .lisp files.

--- skeleton/t/skeleton.lisp
+++ skeleton/t/skeleton.lisp
@@ -1,10 +1,3 @@
-#|
-  This file is a part of <% @var name %> project.
-<%- @if author %>
-  Copyright (c) <%= (local-time:timestamp-year (local-time:now)) %> <% @var author %><% @if email %> (<% @var email %>)<% @endif %>
-<%- @endif %>
-|#
-
 (in-package :cl-user)
 (defpackage <% @var name %>-test
   (:use :cl
