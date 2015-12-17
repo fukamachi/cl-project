04c4a608e436bdaa84eef9229a8211056134d1a8
Eitaro Fukamachi <e.arrows@gmail.com>
2015-12-17 16:50:48 +0900

    Suppress a style-warning.

--- src/cl-project.lisp
+++ src/cl-project.lisp
@@ -15,7 +15,7 @@
 
 (defun make-project (path &rest params &key name description author email license depends-on (without-tests nil) &allow-other-keys)
   "Generate a skeleton."
-  (declare (ignore name description author email license depends-on))
+  (declare (ignore name description author email license depends-on without-tests))
   (check-type path pathname)
 
   ;; Ensure `path' ends with a slash(/).
