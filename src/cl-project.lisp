a7ef3043947a2ac575492d72435df661d08433e1
Serdar Gokcen <home@sgokcen.com>
2018-11-06 04:00:30 +0300

    add-to-init-file function added in order to let newly generated application to be loaded at CL startup.

--- src/cl-project.lisp
+++ src/cl-project.lisp
@@ -10,12 +10,64 @@
                 #:generate)
   (:export #:*skeleton-directory*
            #:make-project
-           #:generate-skeleton))
+           #:generate-skeleton
+           #:add-to-init-file))
 (in-package :cl-project)
 
-(defun make-project (path &rest params &key name description author email license depends-on (without-tests nil) &allow-other-keys)
+(defun get-init-file-name ()
+  "Determines the init file name for different CL implementations."
+  (or
+   #+sbcl
+   ".sbclrc"
+   #+allegro
+   ".clinit.cl"
+   #+abcl
+   ".abclrc"
+   #+(and ccl windows)
+   "ccl-init.lisp"
+   #+(and ccl (not windows))
+   ".ccl-init.lisp"
+   #+clasp
+   ".clasprc"
+   #+clisp
+   ".clisprc.lisp"
+   #+cmu
+   ".cmucl-init.lisp"
+   #+scl
+   ".scl-init.lisp"
+   #+lispworks
+   ".lispworks"
+   #+ecl
+   ".eclrc"
+   #+mkcl
+   ".mkclrc" nil))
+
+(defun add-to-init-file (path &rest params &key name)
+  "Adds the application to the CL implementation's init file to load generated application at every startup of CL."
+  
+  ;; Ensure `path' ends with a slash(/).
+  (setf path (uiop:ensure-directory-pathname path))
+  
+  (unless (getf params :name)
+    (setf (getf params :name)
+          (car (last (pathname-directory path)))))
+  
+  (let ((init-file-name (get-init-file-name)))
+    (when init-file-name
+      (let ((init-file-path (merge-pathnames init-file-name (user-homedir-pathname))))
+	(with-open-file (stream init-file-path 
+				:direction :output 
+				:if-exists :append 
+				:if-does-not-exist :create)
+	  (format stream 
+		  "~%~%~%~A~A~%~A~S~A~%~%~%"
+		  "#-" 
+		  (getf params :name)
+		  "(pushnew " path " asdf:*central-registry*)"))))))
+
+(defun make-project (path &rest params &key name description author email license depends-on (without-tests nil) load-at-startup &allow-other-keys)
   "Generate a skeleton."
-  (declare (ignore name description author email license depends-on without-tests))
+  (declare (ignore name description author email license depends-on without-tests load-at-startup))
   (check-type path pathname)
 
   ;; Ensure `path' ends with a slash(/).
@@ -32,6 +84,8 @@
       (when (string= (pathname-type file) "asd")
         (let ((dir (make-pathname :name nil :type nil :defaults file)))
           (push dir asdf:*central-registry*)))))
+  (when (getf params :load-at-startup)
+    (add-to-init-file path :name (getf params :name)))
   t)
 
 (defun generate-skeleton (source-dir target-dir &key env)
